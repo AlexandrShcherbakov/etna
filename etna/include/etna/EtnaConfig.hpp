78321b1744deecf1ec30b7751a4963f940ad4b75
AlexandrShcherbakov <alex.shcherbakov.as@gmail.com>
2022-10-03 20:36:37 +0300

    Enable dynamic rendering
    VK version was incremented.

--- etna/include/etna/EtnaConfig.hpp
+++ etna/include/etna/EtnaConfig.hpp
@@ -14,7 +14,7 @@ namespace etna
   inline constexpr std::array VALIDATION_LAYERS = {"VK_LAYER_KHRONOS_validation", "VK_LAYER_LUNARG_monitor"};
 #endif
 
-inline constexpr uint32_t VULKAN_API_VERSION = VK_API_VERSION_1_2;
+inline constexpr uint32_t VULKAN_API_VERSION = VK_API_VERSION_1_3;
 
 inline constexpr const char *ETNA_ENGINE_NAME = "etna";
 inline constexpr uint32_t ETNA_VERSION = VK_MAKE_VERSION(0, 1, 0);
