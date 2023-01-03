4a2e863da482761fed7dc186fce87f4faf3c053d
Roman Sandu <roma.sandu@gmail.com>
2023-01-04 00:36:55 +0300

    Fix codestyle in render target states
    we use 2 spaces, not 4

--- etna/include/etna/RenderTargetStates.hpp
+++ etna/include/etna/RenderTargetStates.hpp
@@ -13,22 +13,22 @@ namespace etna
 
 class RenderTargetState
 {
-    VkCommandBuffer commandBuffer;
-    static bool inScope;
+  VkCommandBuffer commandBuffer;
+  static bool inScope;
 public:
-    struct AttachmentParams
-    {
-        // TODO: Add new fields for clearing etc.
-        vk::Image image = VK_NULL_HANDLE;
-        vk::ImageView view = VK_NULL_HANDLE;
-        AttachmentParams() = default;
-        AttachmentParams(vk::Image i, vk::ImageView v) : image(i), view(v) {}
-        AttachmentParams(const Image &img) : image(img.get()), view(img.getView({})) {}
-    };
+  struct AttachmentParams
+  {
+    // TODO: Add new fields for clearing etc.
+    vk::Image image = VK_NULL_HANDLE;
+    vk::ImageView view = VK_NULL_HANDLE;
+    AttachmentParams() = default;
+    AttachmentParams(vk::Image i, vk::ImageView v) : image(i), view(v) {}
+    AttachmentParams(const Image &img) : image(img.get()), view(img.getView({})) {}
+  };
 
-    RenderTargetState(VkCommandBuffer cmd_buff, vk::Extent2D extend,
-    const std::vector<AttachmentParams> &color_attachments, AttachmentParams depth_attachment);
-    ~RenderTargetState();
+  RenderTargetState(VkCommandBuffer cmd_buff, vk::Extent2D extend,
+  const std::vector<AttachmentParams> &color_attachments, AttachmentParams depth_attachment);
+  ~RenderTargetState();
 };
 
 }
