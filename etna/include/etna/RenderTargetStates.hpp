19e7984e51e510c5a4fd46870f94b8a9bbff19f0
kzubatov <zubatovks@mail.ru>
2024-02-10 12:56:24 +0300

    tab fix

--- etna/include/etna/RenderTargetStates.hpp
+++ etna/include/etna/RenderTargetStates.hpp
@@ -13,33 +13,34 @@ namespace etna
 
 class RenderTargetState
 {
-    VkCommandBuffer commandBuffer;
-    static bool inScope;
+  VkCommandBuffer commandBuffer;
+  static bool inScope;
 public:
-    struct AttachmentParams
-    {
-        // TODO: Add new fields for clearing etc.
-        vk::Image image = VK_NULL_HANDLE;
-        vk::ImageView view = VK_NULL_HANDLE;
-        vk::AttachmentLoadOp loadOp = vk::AttachmentLoadOp::eClear;
-        vk::AttachmentStoreOp storeOp = vk::AttachmentStoreOp::eStore;
-        vk::ClearColorValue clearColorValue = std::array<float, 4>({0.0f, 0.0f, 0.0f, 1.0f});
-        vk::ClearDepthStencilValue clearDepthStencilValue = {1.0f, 0};
-        AttachmentParams() = default;
-        AttachmentParams(vk::Image i, vk::ImageView v) : image(i), view(v) {}
-        AttachmentParams(const Image &img) : image(img.get()), view(img.getView({})) {}
-    };
+  struct AttachmentParams
+  {
+    // TODO: Add new fields for clearing etc.
+    vk::Image image = VK_NULL_HANDLE;
+    vk::ImageView view = VK_NULL_HANDLE;
+    vk::AttachmentLoadOp loadOp = vk::AttachmentLoadOp::eClear;
+    vk::AttachmentStoreOp storeOp = vk::AttachmentStoreOp::eStore;
+    vk::ClearColorValue clearColorValue = std::array<float, 4>({0.0f, 0.0f, 0.0f, 1.0f});
+    vk::ClearDepthStencilValue clearDepthStencilValue = {1.0f, 0};
+    AttachmentParams() = default;
+    AttachmentParams(vk::Image i, vk::ImageView v) : image(i), view(v) {}
+    AttachmentParams(const Image &img) : image(img.get()), view(img.getView({})) {}
+  };
     
-    RenderTargetState(VkCommandBuffer cmd_buff, vk::Extent2D extend,
-        const std::vector<AttachmentParams> &color_attachments, AttachmentParams depth_attachment,
-        AttachmentParams stencil_attachment);
-    // We can't use the default argument for stencil_attachment due to gcc bug 88165
-    // See https://gcc.gnu.org/bugzilla/show_bug.cgi?id=88165
-    RenderTargetState(VkCommandBuffer cmd_buff, vk::Extent2D extend, 
-        const std::vector<AttachmentParams> &color_attachments, AttachmentParams depth_attachment) 
-        : RenderTargetState(cmd_buff, extend, color_attachments, depth_attachment, {}) {};
+  RenderTargetState(VkCommandBuffer cmd_buff, vk::Extent2D extend,
+    const std::vector<AttachmentParams> &color_attachments, AttachmentParams depth_attachment,
+    AttachmentParams stencil_attachment);
 
-    ~RenderTargetState();
+  // We can't use the default argument for stencil_attachment due to gcc bug 88165
+  // See https://gcc.gnu.org/bugzilla/show_bug.cgi?id=88165
+  RenderTargetState(VkCommandBuffer cmd_buff, vk::Extent2D extend, 
+    const std::vector<AttachmentParams> &color_attachments, AttachmentParams depth_attachment) 
+    : RenderTargetState(cmd_buff, extend, color_attachments, depth_attachment, {}) {};
+
+  ~RenderTargetState();
 };
 
 }
