1cd36b24b45ba624d82c59d80d70a216ea11ff53
Roman Sandu <roma.sandu@gmail.com>
2024-07-28 04:30:52 +0300

    Command manager abstraction
    Command manager is a bad name, but I don't have a better one currently. This is basically "the other half" of Window which is responsible for actual commands we execute on the GPU.
    When this is used in conjunction with the Window class, we get a simple draw loop: get a command buffer, acquire swapchain image, submit the command buffer, present the swapchain image.
    All GPU-CPU and GPU-GPU frame sync logic is now handled by these two classes for simple applications.

---
+++ etna/include/etna/CommandManager.hpp
@@ -0,0 +1,69 @@
+#pragma once
+#ifndef ETNA_COMMAND_POOL_HPP_INCLUDED
+#define ETNA_COMMAND_POOL_HPP_INCLUDED
+
+#include <optional>
+
+#include <etna/Vulkan.hpp>
+#include <etna/GpuWorkCount.hpp>
+#include <etna/GpuSharedResource.hpp>
+
+
+namespace etna
+{
+
+/**
+ * Simple manager of command buffers. Provides a single command buffer per
+ * frame and provides a simple API to submit it to the relevant queue every frame.
+ * Suboptimal for many reasons, so feel free to read the implementation and be
+ * inspired by it.
+ */
+class CommandManager
+{
+public:
+  struct Dependencies
+  {
+    const GpuWorkCount& workCount;
+    vk::Device device;
+
+    vk::Queue submitQueue;
+    std::uint32_t queueFamily;
+  };
+
+  explicit CommandManager(const Dependencies& deps);
+
+  /**
+   * Acquires the command buffer to use this frame, waiting for
+   * the GPU to complete previous multi-buffering-count frames if necessary.
+   */
+  vk::CommandBuffer acquireNext();
+
+  /**
+   * Submits the command buffer acquired from acquire(), but allows it to
+   * write to color attachments only after the `after` semaphore is signaled.
+   * Intended to be used with a semaphore that signals availability of the
+   * swap chain image used in the buffer (see etna::Window::acquireNext).
+   * Returns a semaphore that is signaled when GPU has done executing the buffer,
+   * which is intended to be used for presenting the swap chain image (see etna::Window::present).
+   */
+  vk::Semaphore submit(vk::CommandBuffer what, vk::Semaphore write_attachments_after);
+
+private:
+  const GpuWorkCount& workCount;
+  vk::Device device;
+
+  vk::Queue submitQueue;
+
+  vk::UniqueCommandPool pool;
+  GpuSharedResource<vk::UniqueFence> commandsComplete;
+
+  // NOTE: semaphores are GPU-only resources, no need to multi-buffer them.
+  vk::UniqueSemaphore gpuDone;
+
+  std::optional<GpuSharedResource<vk::UniqueCommandBuffer>> buffers;
+};
+
+} // namespace etna
+
+
+#endif // ETNA_COMMAND_POOL_HPP_INCLUDED
---
+++ etna/source/CommandManager.cpp
@@ -0,0 +1,80 @@
+#include <etna/CommandManager.hpp>
+
+namespace etna
+{
+
+CommandManager::CommandManager(const Dependencies &deps)
+  : workCount{deps.workCount}
+  , device{deps.device}
+  , submitQueue{deps.submitQueue}
+  , pool{unwrap_vk_result(deps.device.createCommandPoolUnique(vk::CommandPoolCreateInfo{
+    .flags = vk::CommandPoolCreateFlagBits::eResetCommandBuffer,
+    .queueFamilyIndex = deps.queueFamily,
+  }))}
+  , commandsComplete{deps.workCount, [&deps](std::size_t){
+    return unwrap_vk_result(deps.device.createFenceUnique(vk::FenceCreateInfo{ .flags = vk::FenceCreateFlagBits::eSignaled }));
+  }}
+  , gpuDone{unwrap_vk_result(deps.device.createSemaphoreUnique({}))}
+{
+  vk::CommandBufferAllocateInfo cbInfo{
+    .commandPool = pool.get(),
+    .level = vk::CommandBufferLevel::ePrimary,
+    .commandBufferCount = static_cast<uint32_t>(deps.workCount.multiBufferingCount()),
+  };
+  auto bufsVec = unwrap_vk_result(deps.device.allocateCommandBuffersUnique(cbInfo));
+  buffers.emplace(deps.workCount, [&bufsVec](std::size_t i) { return std::move(bufsVec[i]); });
+}
+
+vk::CommandBuffer CommandManager::acquireNext()
+{
+  // Wait for previous execution of the current command
+  // buffer to complete. It may in fact already be long
+  // finished, but we still have to synchronize GPU and CPU
+  // explicitly. This also synchronizes all other shared
+  // resources which live inside GpuSharedResource containers.
+  auto curComplete = commandsComplete.get().get();
+  ETNA_CHECK_VK_RESULT(device.waitForFences({curComplete}, vk::True, 1000000));
+  device.resetFences({curComplete});
+
+  auto curBuf = buffers->get().get();
+  curBuf.reset();
+
+  return curBuf;
+}
+
+vk::Semaphore CommandManager::submit(vk::CommandBuffer what, vk::Semaphore write_attachments_after)
+{
+  // NOTE: the only point in passing in `what` here is for
+  // symmetry an aesthetic reasons.
+  ETNA_ASSERT(what == buffers->get().get());
+  std::array cbsInfo{vk::CommandBufferSubmitInfo{
+    .commandBuffer = what,
+    .deviceMask = 1,
+  }};
+
+  std::array wait{vk::SemaphoreSubmitInfo{
+    .semaphore = write_attachments_after,
+    .stageMask = vk::PipelineStageFlagBits2::eColorAttachmentOutput,
+    .deviceIndex = 1,
+  }};
+
+  // NOTE: this is intended to be used for presenting, and as far
+  // as I can tell, stageMask cannot do anything sensible on HW level
+  // here. Also, there are outstanding issues about this in VK spec, so...
+  // https://github.com/KhronosGroup/Vulkan-Docs/issues/1308
+  std::array signal{vk::SemaphoreSubmitInfo{
+    .semaphore = gpuDone.get(),
+    .stageMask = {},
+    .deviceIndex = 1,
+  }};
+
+  vk::SubmitInfo2 sInfo{};
+  sInfo.setCommandBufferInfos(cbsInfo);
+  sInfo.setWaitSemaphoreInfos(wait);
+  sInfo.setSignalSemaphoreInfos(signal);
+  ETNA_CHECK_VK_RESULT(submitQueue.submit2({sInfo}, commandsComplete.get().get()));
+
+  return gpuDone.get();
+}
+
+} // namespace etna
