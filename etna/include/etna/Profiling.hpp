cb88ac5ac484ca35bc3374970e5b771decbc9585
Mrkol <roma.sandu@gmail.com>
2024-10-07 00:15:15 +0300

    Silence GCC 12 false-positive maybe-uninitialized

--- etna/include/etna/Profiling.hpp
+++ etna/include/etna/Profiling.hpp
@@ -2,10 +2,17 @@
 #ifndef ETNA_PROFILING_HPP_INCLUDED
 #define ETNA_PROFILING_HPP_INCLUDED
 
+// NOTE: either gcc 12 has a false-positive here, or it's a Tracy problem.
+#if __GNUC__ && !__clang__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+#endif
+
 #include <etna/Vulkan.hpp>
 #include <tracy/Tracy.hpp>
 #include <tracy/TracyVulkan.hpp>
 #include <etna/GlobalContext.hpp>
+
 
 // Actually profiles both CPU and GPU. Feel free to sprinkle all over your code.
 // This is a scoped macro, so a profiling region ends at the end of a scope.
@@ -18,5 +25,9 @@
 // If you have a TON of profiling events, must be called more frequently.
 #define ETNA_READ_BACK_GPU_PROFILING(cmdBuf)                                                       \
   TracyVkCollect(reinterpret_cast<TracyVkCtx>(etna::get_context().getTracyContext()), (cmdBuf))
+
+#if __GNUC__ && !__clang__
+#pragma GCC diagnostic pop
+#endif
 
 #endif // ETNA_PROFILING_HPP_INCLUDED
