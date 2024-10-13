5df43666ca78f9b68509ab4040b77fe5e48037cb
Roman Sandu <roma.sandu@gmail.com>
2024-10-14 01:00:58 +0300

    Misc Image/Buffer improvements
    1) Restored backward compat due to missed initializer
    2) Added some comments because why not
    3) Fixed Bits -> Flags for view aspect
    4) Use vulkan.hpp stuff instead of vulkan.h

--- etna/include/etna/Buffer.hpp
+++ etna/include/etna/Buffer.hpp
@@ -17,11 +17,22 @@ class Buffer
 public:
   Buffer() = default;
 
+  // Settings for creating a new buffer
   struct CreateInfo
   {
+    // Size of the buffer in bytes
     vk::DeviceSize size;
+
+    // How will this buffer be used?
     vk::BufferUsageFlags bufferUsage = vk::BufferUsageFlagBits::eStorageBuffer;
+
+    // Basically determines the memory type this buffer will live in.
+    // You want GPU_ONLY for stuff that is produced on the GPU or read very often
+    // on the GPU and not updated from the CPU all that often (and update it via copies).
+    // Otherwise, feel free to use CPU_TO_GPU or GPU_TO_CPU e.g. for uniform buffers.
     VmaMemoryUsage memoryUsage = VMA_MEMORY_USAGE_GPU_ONLY;
+
+    // Name of the image for debugging tools
     std::string_view name;
   };
 
@@ -39,7 +50,12 @@ public:
 
   BufferBinding genBinding(vk::DeviceSize offset = 0, vk::DeviceSize range = VK_WHOLE_SIZE) const;
 
+  // If the buffer is in CPU_TO_GPU or GPU_TO_CPU memory, returns a CPU-accessible
+  // pointer to the start of this buffer's bytes, which can be used for reading or
+  // writing the buffer (preferably in a linear manner).
   std::byte* map();
+
+  // Invalidates the pointer returned by map.
   void unmap();
 
   ~Buffer();
--- etna/include/etna/Image.hpp
+++ etna/include/etna/Image.hpp
@@ -17,23 +17,54 @@ class Image
 public:
   Image() = default;
 
+  // Images are a very versatile tool in graphics and have a TON of settings,
+  // which this structure specifies
   struct CreateInfo
   {
+    // Size of the image (yes, 3D images are allowed if you specify the correct type)
     vk::Extent3D extent;
+
+    // Name of the image for debugging tools
     std::string_view name;
+
     // NOTE: this format is the default for TEXTURE ASSETS,
     // if you are using the image as a render target, you almost
     // definitely want UNorm.
     vk::Format format = vk::Format::eR8G8B8A8Srgb;
+
+    // How will this image be used?
     vk::ImageUsageFlags imageUsage =
       vk::ImageUsageFlagBits::eColorAttachment | vk::ImageUsageFlagBits::eSampled;
+
+    // Basically determines the memory type this texture will live in.
+    // You almost definitely almost always want GPU-only memory.
     VmaMemoryUsage memoryUsage = VMA_MEMORY_USAGE_GPU_ONLY;
+
+    // Images are stored in "chunks" on the GPU, which is called "optimal tiling".
+    // The way these chunks work is platform-dependent and unspecified, so you
+    // cannot work with the image's memory without going through Vulkan's APIs.
+    // If you chose linear here, the pixels will be stored by rows and so you
+    // will be able to correctly interpret the image's memory even without Vulkan,
+    // but this will make the GPU work with the image significantly slower.
     vk::ImageTiling tiling = vk::ImageTiling::eOptimal;
+
+    // You can do "arrays of images within a single image" by specifying layers > 1
     std::size_t layers = 1;
+
+    // Amount of mip levels for this image.
+    // TODO: allow specifying 0 for auto calculation of mips from extent.
     std::size_t mipLevels = 1;
+
+    // HW-supported MSAA stuff, don't touch if you don't know what MSAA is.
+    // Note that you are only allowed to specify a single flag here.
     vk::SampleCountFlagBits samples = vk::SampleCountFlagBits::e1;
+
+    // Type of image: 1D array-of-pixels, simple 2D image or 3D volumetric image.
     vk::ImageType type = vk::ImageType::e2D;
-    vk::ImageCreateFlags flags;
+
+    // Additional flags, primary usage being allowing for creation of cube or array
+    // views to array textures (for cube, specify 6 layers).
+    vk::ImageCreateFlags flags = {};
   };
 
   Image(VmaAllocator alloc, CreateInfo info);
@@ -50,24 +81,41 @@ public:
   ~Image();
   void reset();
 
+  // Parameters for creating views which specify how the image contents should be
+  // interpreted and which part of the image we want to "view"
   struct ViewParams
   {
+    // First mip levels to view
     uint32_t baseMip = 0;
-    uint32_t levelCount = VK_REMAINING_MIP_LEVELS;
+
+    // Count of mip levels to view (default = all)
+    uint32_t levelCount = vk::RemainingMipLevels;
+
+    // First layer to view
     uint32_t baseLayer = 0;
-    uint32_t layerCount = VK_REMAINING_ARRAY_LAYERS;
-    std::optional<vk::ImageAspectFlagBits> aspectMask{};
-    std::optional<vk::ImageViewType> type{};
+
+    // Count of layers to view (default = all)
+    uint32_t layerCount = vk::RemainingArrayLayers;
+
+    // "Aspects" of the image to view. Is only useful for combined depth/stencil
+    // images, by default is determined automatically as all present aspects.
+    std::optional<vk::ImageAspectFlags> aspectMask = std::nullopt;
+
+    // The way we interpret the image: 1/2/3D, cube, array, etc.
+    // By default views it as 1/2/3D automatically based on the image itself.
+    std::optional<vk::ImageViewType> type = std::nullopt;
 
     bool operator==(const ViewParams& b) const = default;
   };
   vk::ImageView getView(ViewParams params) const;
 
+  // Creates a binding to be used with etna::Binding and etna::create_descriptor_set
   ImageBinding genBinding(
     vk::Sampler sampler,
     vk::ImageLayout layout,
     ViewParams params = {0, VK_REMAINING_MIP_LEVELS, 0, VK_REMAINING_ARRAY_LAYERS, {}, {}}) const;
 
+  // Returns the "all" aspects combination of flags based on the image's real format
   vk::ImageAspectFlags getAspectMaskByFormat() const;
 
   vk::Extent3D getExtent() const { return extent; }
